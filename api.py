8067347491896d587671fdf719bdf5c409b571fb
Joseph Woolley <joe@calledtoconstruct.net>
2018-12-08 18:45:44 -0500

    Implemented flask web service to display random image.

--- api.py
+++ api.py
@@ -2,6 +2,8 @@
 import requests
 import json
 
+from flask import Flask
+
 def get_random_maltese():
     response = requests.get('https://dog.ceo/api/breed/maltese/images/random')
     text = response.text
@@ -9,3 +11,18 @@ def get_random_maltese():
     message = data["message"]
     status_code = 200
     return status_code, message
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+
+    status_code, response = get_random_maltese()
+
+    img = '<img src="' + response + '" />'
+
+    body = '<body>' + img + '</body>'
+    head = '<head>' + '</head>'
+    html = '<html>' + head + body + '</html>'
+    
+    return html
--- tests/test_api.py
+++ tests/test_api.py
@@ -3,10 +3,6 @@ import pytest
 
 from api import get_random_maltese
 
-# def test_given_a_working_internet_connection_when_requesting_dogs_then_ok_is_returned():
-#     dog = get_maltese()
-#     assert(dog.status_code == 200)
-
 def test_given_a_working_internet_connection_when_requesting_dogs_then_ok_is_returned():
     status_code, dog = get_random_maltese()
     expected_value = 200
